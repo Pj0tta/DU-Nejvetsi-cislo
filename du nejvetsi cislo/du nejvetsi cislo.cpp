a5e84352fe2a9ddd57fccaa69ab1e81cb9b09ee4
Petr Habel <petrhabel04@gmail.com>
2021-10-30 14:21:37 +0200

    Přidejte soubory projektu.

---
+++ du nejvetsi cislo/du nejvetsi cislo.cpp
@@ -0,0 +1,45 @@
+﻿// du nejvetsi cislo.cpp : Tento soubor obsahuje funkci main. Provádění programu se tam zahajuje a ukončuje.
+//
+
+#include <iostream>
+
+int main()
+{
+	int x = 9, y = 69, z = 53;
+	int max;
+
+	if (x > y && x > z) {
+		max = x;
+	}
+	else {
+		if (y > x && y > z) {
+			max = y;
+		}
+		else {
+			if (z > x && z > y) {
+				max = z;
+			}
+			else {
+				if (x == y && y == z) {
+					max = x;
+				}
+			}
+		}
+	}
+	printf("Nejvetsi cislo je %d", max);
+
+
+
+   
+}
+
+// Spuštění programu: Ctrl+F5 nebo nabídka Ladit > Spustit bez ladění
+// Ladění programu: F5 nebo nabídka Ladit > Spustit ladění
+
+// Tipy pro zahájení práce:
+//   1. K přidání nebo správě souborů použijte okno Průzkumník řešení.
+//   2. Pro připojení ke správě zdrojového kódu použijte okno Team Explorer.
+//   3. K zobrazení výstupu sestavení a dalších zpráv použijte okno Výstup.
+//   4. K zobrazení chyb použijte okno Seznam chyb.
+//   5. Pokud chcete vytvořit nové soubory kódu, přejděte na Projekt > Přidat novou položku. Pokud chcete přidat do projektu existující soubory kódu, přejděte na Projekt > Přidat existující položku.
+//   6. Pokud budete chtít v budoucnu znovu otevřít tento projekt, přejděte na Soubor > Otevřít > Projekt a vyberte příslušný soubor .sln.
